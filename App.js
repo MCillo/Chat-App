e31cc51abc64cbd7cb3af11b2a2505f4ffce9ffe
Michael Cillo <117599499+MCillo@users.noreply.github.com>
2023-10-08 15:53:15 -0400

    Update App.js
    created initial app.js file
    imported React Navigation
    imported Screen 1
    imported Screen 2
    created navigator
    created Screen 1 DOM
    created Screen 2 DOM

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+// import react navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// importing screens
+import Screen1 from './components/Screen1';
+import Screen2 from './components/Screen2';
+
+// create the navigator
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Screen1'>
+        <Stack.Screen
+          name='Screen1'
+          component={Screen1}
+        />
+        <Stack.Screen
+          name='Screen2'
+          component={Screen2}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -18,3 +37,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
