88fc6c24d1d1ab1d99a915256150534b263a69a3
Michael Cillo <117599499+MCillo@users.noreply.github.com>
2023-10-19 15:47:07 -0400

    Update Chat.js
    added collection, getDocs, addDoc, onSnapshot, query, where from firebase/firestore
    added orderBy from lodash
    added userID to route.params
    added getMessages query

--- components/Chat.js
+++ components/Chat.js
@@ -1,65 +1,74 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, View, Text, KeyboardAvoidingView, Platform } from "react-native";
+import { StyleSheet, View, Text, KeyboardAvoidingView, Platform, TouchableOpacity, Alert } from "react-native";
 import { Bubble, GiftedChat } from "react-native-gifted-chat";
+import { collection, getDocs, addDoc, onSnapshot, query, where } from "firebase/firestore";
+import { orderBy } from "lodash";
 
-const ChatScreen = ({ route, navigation }) => {
+const ChatScreen = ({ route, navigation, db }) => {
 
-  const { name, backgroundColor } = route.params;
+  const { name, backgroundColor, userID } = route.params;
   const [messages, setMessages] = useState([]);
+
+
 
   useEffect(() => {
     navigation.setOptions({ title: name })
-    setMessages([
-      {
-        _id: 1,
-        text: "Hello " + name,
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: "React Native",
-          avatar: "https://placeimg.com/140/140/any",
-        },
-      },
-      {
-        _id: 2,
-        text: 'This is a system message!',
-        createdAt: new Date(),
-        system: true,
-      },
-    ]);
+
+    const getMessagesQuery = query(collection(db, 'messages'), orderBy('createdAt', 'desc'));
+    const unsubMessages = onSnapshot
+    setMessages(newMessages)
+  }
+    // setMessages([
+    //   {
+    //     _id: 1,
+    //     text: "Hello " + name,
+    //     createdAt: new Date(),
+    //     user: {
+    //       _id: 2,
+    //       name: "React Native",
+    //       avatar: "https://placeimg.com/140/140/any",
+    //     },
+    //   },
+    //   {
+    //     _id: 2,
+    //     text: 'This is a system message!',
+    //     createdAt: new Date(),
+    //     system: true,
+    //   },
+    // ]);
   }, []);
 
-  const onSend = (newMessages) => {
-    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
-  }
+const onSend = (newMessages) => {
+  setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+}
 
-  const renderBubble = (props) => {
-    return <Bubble
-      {...props}
-      wrapperStyle={{
-        right: {
-          backgroundColor: '#000'
-        },
-        left: {
-          backgroundColor: '#fff'
-        }
+const renderBubble = (props) => {
+  return <Bubble
+    {...props}
+    wrapperStyle={{
+      right: {
+        backgroundColor: '#000'
+      },
+      left: {
+        backgroundColor: '#fff'
+      }
+    }}
+  />
+}
+
+return (
+  <View style={{ flex: 1, backgroundColor: backgroundColor }}>
+    <GiftedChat
+      messages={messages}
+      renderBubble={renderBubble}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1
       }}
     />
-  }
-
-  return (
-    <View style={{ flex: 1, backgroundColor: backgroundColor }}>
-      <GiftedChat
-        messages={messages}
-        renderBubble={renderBubble}
-        onSend={messages => onSend(messages)}
-        user={{
-          _id: 1
-        }}
-      />
-      {Platform.OS === 'android' ? <KeyboardAvoidingView behavior='height' /> : null}
-    </View>
-  )
+    {Platform.OS === 'android' ? <KeyboardAvoidingView behavior='height' /> : null}
+  </View>
+)
 
 }
 
