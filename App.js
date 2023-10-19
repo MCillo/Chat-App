94de4f4c473af46330a3e8bc4b6feea50ae0deac
Michael Cillo <117599499+MCillo@users.noreply.github.com>
2023-10-19 15:44:05 -0400

    Update App.js
    added initializeApp and getFirestore for interacting with DB

--- App.js
+++ App.js
@@ -5,14 +5,40 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+// create the navigator
+const Stack = createNativeStackNavigator();
+
+// initializing the app and importing firestore for storing the chat messages
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
 // importing screens
 import StartScreen from './components/Start';
 import ChatScreen from './components/Chat';
 
-// create the navigator
-const Stack = createNativeStackNavigator();
+// logbox for ignoring warning alerts
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['AsyncStorage has been extracted from']);
+
 
 const App = () => {
+  // Chat-App firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAtUJG-F_rSjX871Xkpni5Kd7dR5Udggqs",
+    authDomain: "chatapp-3d111.firebaseapp.com",
+    projectId: "chatapp-3d111",
+    storageBucket: "chatapp-3d111.appspot.com",
+    messagingSenderId: "166674648514",
+    appId: "1:166674648514:web:d4ab1aa37cdecfda541a5b",
+    measurementId: "G-Y3P3V592J2"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Start'>
@@ -22,8 +48,9 @@ const App = () => {
         />
         <Stack.Screen
           name='ChatScreen'
-          component={ChatScreen}
-        />
+        >
+          {props => <ChatScreen db={db}{...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
