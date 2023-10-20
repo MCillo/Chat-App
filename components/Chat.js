8b864333240884e2232051dfdb752daa6ed83e94
MCillo <117599499+MCillo@users.noreply.github.com>
2023-10-20 05:53:55 -0400

    Update Chat.js
    tweaked code

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import { StyleSheet, View, Text, KeyboardAvoidingView, Platform, TouchableOpacity, Alert } from "react-native";
 import { Bubble, GiftedChat } from "react-native-gifted-chat";
-import { collection, getDocs, addDoc, onSnapshot, query, where } from "firebase/firestore";
-import { orderBy } from "lodash";
+import { collection, getDocs, addDoc, onSnapshot, query, where, orderBy } from "firebase/firestore";
 
 const ChatScreen = ({ route, navigation, db }) => {
 
@@ -14,63 +13,58 @@ const ChatScreen = ({ route, navigation, db }) => {
   useEffect(() => {
     navigation.setOptions({ title: name })
 
+
     const getMessagesQuery = query(collection(db, 'messages'), orderBy('createdAt', 'desc'));
-    const unsubMessages = onSnapshot
-    setMessages(newMessages)
-  }
-    // setMessages([
-    //   {
-    //     _id: 1,
-    //     text: "Hello " + name,
-    //     createdAt: new Date(),
-    //     user: {
-    //       _id: 2,
-    //       name: "React Native",
-    //       avatar: "https://placeimg.com/140/140/any",
-    //     },
-    //   },
-    //   {
-    //     _id: 2,
-    //     text: 'This is a system message!',
-    //     createdAt: new Date(),
-    //     system: true,
-    //   },
-    // ]);
+    const unsubMessages = onSnapshot(getMessagesQuery, (docs) => {
+      let newMessages = [];
+      docs.forEach(doc => {
+        newMessages.push({
+          id: doc.id,
+          ...doc.data(),
+          createdAt: new Date(doc.data().createdAt.toMillis())
+        })
+      })
+      setMessages(newMessages)
+    })
+    return () => {
+      if (unsubMessages) unsubMessages();
+    }
   }, []);
 
-const onSend = (newMessages) => {
-  setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
-}
+  const onSend = (newMessages) => {
+    addDoc(collection(db, 'messages'), newMessages[0]);
+  }
 
-const renderBubble = (props) => {
-  return <Bubble
-    {...props}
-    wrapperStyle={{
-      right: {
-        backgroundColor: '#000'
-      },
-      left: {
-        backgroundColor: '#fff'
-      }
-    }}
-  />
-}
-
-return (
-  <View style={{ flex: 1, backgroundColor: backgroundColor }}>
-    <GiftedChat
-      messages={messages}
-      renderBubble={renderBubble}
-      onSend={messages => onSend(messages)}
-      user={{
-        _id: 1
+  const renderBubble = (props) => {
+    return <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: '#000'
+        },
+        left: {
+          backgroundColor: '#fff'
+        }
       }}
     />
-    {Platform.OS === 'android' ? <KeyboardAvoidingView behavior='height' /> : null}
-  </View>
-)
+  }
 
-}
+  return (
+    <View style={{ flex: 1, backgroundColor: backgroundColor }}>
+      <GiftedChat
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: userID,
+          name,
+        }}
+      />
+      {Platform.OS === 'android' ? <KeyboardAvoidingView behavior='height' /> : null}
+    </View>
+  )
+
+};
 
 const styles = StyleSheet.create({
   container: {
